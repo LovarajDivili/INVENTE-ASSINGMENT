18700b8e2fbcc88fd74a949527f5d37db1c8ee89
LovarajDivili <lovarajudivili1668@gmail.com>
2022-07-05 08:07:16 +0000

    first commit

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,30 +1,35 @@
 import {Link} from 'react-router-dom'
 
 import './index.css'
+import {Component} from 'react'
 
-const Header = () => (
-  <nav className="nav-header">
-    <div className="blog-container">
-      <h1 className="blog-title">Videos App</h1>
-      <ul className="nav-menu">
-        <li>
-          <Link className="nav-link" to="/">
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link className="nav-link" to="/about">
-            About
-          </Link>
-        </li>
-        <li>
-          <Link className="nav-link" to="/contact">
-            Contact
-          </Link>
-        </li>
-      </ul>
-    </div>
-  </nav>
-)
+class Header extends Component {
+  render() {
+    return (
+      <nav className="nav-header">
+        <div className="blog-container">
+          <h1 className="blog-title">Videos App</h1>
+          <ul className="nav-menu">
+            <li>
+              <Link className="nav-link" to="/">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link className="nav-link" to="/about">
+                About
+              </Link>
+            </li>
+            <li>
+              <Link className="nav-link" to="/contact">
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    )
+  }
+}
 
 export default Header
--- src/components/VideoList/index.js
+++ src/components/VideoList/index.js
@@ -71,10 +71,41 @@ const VideosList = [
 ]
 
 class VideoList extends Component {
+  state = {
+    searchInput: '',
+  }
+
+  onSearchInput = event => {
+    this.setState({searchInput: event.target.value})
+  }
+
   render() {
+    const {searchInput} = this.state
+    const searchResults = VideosList.filter(each =>
+      each.title.includes(searchInput),
+    )
     return (
       <div className="blog-list-container">
-        {VideosList.map(eachVideo => (
+        <div>
+          <p>Search Your favorite Content Videos...!</p>
+          <input
+            type="search"
+            className="search"
+            value={searchInput}
+            onChange={this.onSearchInput}
+          />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            className="icon_search"
+            viewBox="0 0 16 16"
+          >
+            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+          </svg>
+        </div>
+        {searchResults.map(eachVideo => (
           <VideoDetails blogData={eachVideo} key={eachVideo.id} />
         ))}
       </div>
